f0670cc72a8e4429c79626e361868c6bd4054036
xingnana <xingnana@meizu.com>
2018-12-20 16:59:35 +0800

    参数化测试

---
+++ src/test/java/com/xnn/parameterTest/ParameterDataProviderITestContextTest.java
@@ -0,0 +1,11 @@
+package com.xnn.parameterTest;
+
+/**
+ * Created with IDEA
+ * Author:Nana Xing
+ * Date:2018/12/20  15:00
+ * Description:
+ * Modified By:
+ */
+public class ParameterDataProviderITestContextTest {
+}
---
+++ src/test/java/com/xnn/util/CharUtilsTest.java
@@ -0,0 +1,11 @@
+package com.xnn.util;
+
+/**
+ * Created with IDEA
+ * Author:Nana Xing
+ * Date:2018/12/20  15:36
+ * Description:
+ * Modified By:
+ */
+public class CharUtilsTest {
+}
