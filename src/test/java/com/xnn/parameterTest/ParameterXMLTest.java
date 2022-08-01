65f48c48f7b5c8862823619c69e08e2f450ab9f9
xingnn <xingnn@tuya.com>
2022-08-01 16:40:33 +0800

    feat: formate code

--- src/test/java/com/xnn/parameterTest/ParameterXMLTest.java
+++ src/test/java/com/xnn/parameterTest/ParameterXMLTest.java
@@ -21,62 +21,63 @@ import java.util.Properties;
  */
 public class ParameterXMLTest {
 
-        Connection connection;
+    Connection connection;
 
     /**
-     *  通过XML 和 @Parameters将参数传递给@Test方法
+     * 通过XML 和 @Parameters将参数传递给@Test方法
+     *
      * @param dbConfig
      * @param poolSize
      */
     @Test
-        @Parameters({"dbConfig","poolSize"})
-        public void createConncetion(String dbConfig,int poolSize){
-            System.out.println("数据库配置文件："+dbConfig);
-            System.out.println("连接池大小："+poolSize);
+    @Parameters({"dbConfig", "poolSize"})
+    public void createConncetion(String dbConfig, int poolSize) {
+        System.out.println("数据库配置文件：" + dbConfig);
+        System.out.println("连接池大小：" + poolSize);
 
-            Properties prop=new Properties();
-            InputStream input=null;
+        Properties prop = new Properties();
+        InputStream input = null;
 
-            try {
-                //第一种方式：从项目的类路径下加载属性文件，则jdbc.properties需要放置在于pom.xml同级目录下
-                //System.getProperty("user.dir") 获取项目路径
-                //String path=System.getProperty("user.dir")+ "\\"+dbConfig;
-                //prop.load(new FileInputStream(dbConfig));
+        try {
+            //第一种方式：从项目的类路径下加载属性文件，则jdbc.properties需要放置在于pom.xml同级目录下
+            //System.getProperty("user.dir") 获取项目路径
+            //String path=System.getProperty("user.dir")+ "\\"+dbConfig;
+            //prop.load(new FileInputStream(dbConfig));
 
-                String packagePath = this.getClass().getResource("").getPath();
+            String packagePath = this.getClass().getResource("").getPath();
 
-                //第二种方式 类的根路径classpath
-                input=this.getClass().getClassLoader().getResourceAsStream(dbConfig);
-                prop.load(input);
+            //第二种方式 类的根路径classpath
+            input = this.getClass().getClassLoader().getResourceAsStream(dbConfig);
+            prop.load(input);
 
-                String driver=prop.getProperty("jdbc.driver");
-                String url=prop.getProperty("jdbc.url");
-                String username=prop.getProperty("jdbc.username");
-                String password=prop.getProperty("jdbc.password");
+            String driver = prop.getProperty("jdbc.driver");
+            String url = prop.getProperty("jdbc.url");
+            String username = prop.getProperty("jdbc.username");
+            String password = prop.getProperty("jdbc.password");
 
-                System.out.println("drivers : " + driver);
-                System.out.println("connectionURL : " + url);
-                System.out.println("username : " + username);
-                System.out.println("password : " + password);
+            System.out.println("drivers : " + driver);
+            System.out.println("connectionURL : " + url);
+            System.out.println("username : " + username);
+            System.out.println("password : " + password);
 
-                Class.forName(driver);
-                connection= DriverManager.getConnection(url,username,password);
-                System.out.println("数据库连接："+connection);
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("数据库连接：" + connection);
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }finally {
-                if (input!=null){
-                    try {
-                        input.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
+    }
 }
